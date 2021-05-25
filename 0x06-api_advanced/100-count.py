5f0ef11c82e21b84c2feaa0957ac597ae11cdf08
Bnaimy <36247291+Bnaimy@users.noreply.github.com>
2021-05-25 15:21:52 +0100

    Update 100-count.py

--- 0x06-api_advanced/100-count.py
+++ 0x06-api_advanced/100-count.py
@@ -1,11 +1,13 @@
 #!/usr/bin/python3
-"""
+"""Function to count words in all hot posts of a given Reddit subreddit."""
+
 import requests
-"""
-Qyery 
 
 
 def count_words(subreddit, word_list, word_count={}, after=None):
+    """Queries the Reddit API and returns the count of words in
+    word_list in the titles of all the hot posts
+    of the subreddit"""
     import requests
 
     sub_info = requests.get("https://www.reddit.com/r/{}/hot.json"
