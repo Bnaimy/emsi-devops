8a4bec8a26f40341925b349371d748c10fbba8f5
Bnaimy <36247291+Bnaimy@users.noreply.github.com>
2021-05-25 15:56:06 +0100

    Update 100-count.py

--- 0x06-api_advanced/100-count.py
+++ 0x06-api_advanced/100-count.py
@@ -23,24 +23,3 @@ import requests
              .get("children")]
     if not hotL:
         return None
-
-    word_list = list(dict.fromkeys(word_list))
-
-    if word_count == {}:
-        word_count = {word: 0 for word in word_list}
-
-    for title in hotL:
-        split_words = title.split(' ')
-        for word in word_list:
-            for s_word in split_words:
-                if s_word.lower() == word.lower():
-                    word_count[word] += 1
-
-    if not inf.get("data").get("after"):
-        sorted_counts = sorted(word_count.items(), key=lambda kv: kv[0])
-        sorted_counts = sorted(word_count.items(),
-                               key=lambda kv: kv[1], reverse=True)
-        [print('{}: {}'.format(k, v)) for k, v in sorted_counts if v != 0]
-    else:
-        return count_words(subreddit, word_list, word_count,
-                           inf.get("data").get("after"))
